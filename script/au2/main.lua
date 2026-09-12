abe6a92eb07d97d7ef2b7cdb03b38b9d524e189e
sigma-axis <132639613+sigma-axis@users.noreply.github.com>
2026-09-13 07:38:01 +0900

    Split files in use of aulua.

---
+++ script/au2/main.lua
@@ -0,0 +1,37 @@
+--information:PixelSnap_S ${PACKAGE_VERSION} by ${AUTHOR}
+--label:配置
+--require:${LEAST_AVIUTL_VERSION}
+local obj, math = obj, math;
+local cx0, cy0, _ = obj.getvalue("center");
+cx0, cy0 = obj.cx + cx0 + obj.w / 2, obj.cy + cy0 + obj.h / 2;
+local cx, cy = math.floor(0.5 + cx0 + cy0), math.floor(0.5 + cx0 - cy0);
+cx, cy = (cx + cy) / 2, (cx - cy) / 2;
+
+local sx, sy, sz = obj.getvalue("scale");
+sx, sy, sz = obj.sx * sx, obj.sy * sy, obj.sz * sz;
+
+local rx, ry, rz = obj.getvalue("angle");
+rx, ry, rz =
+	2 * math.pi * (((obj.rx + rx) / 360) % 1),
+	2 * math.pi * (((obj.ry + ry) / 360) % 1),
+	2 * math.pi * (((obj.rz + rz) / 360) % 1);
+local c_x, s_x, c_y, s_y, c_z, s_z =
+	math.cos(rx), math.sin(rx), math.cos(ry), math.sin(ry), math.cos(rz), math.sin(rz);
+
+local x0, y0, _ = obj.getvalue("pos");
+x0, y0 =
+	x0 + obj.ox + obj.screen_w / 2,
+	y0 + obj.oy + obj.screen_h / 2;
+
+local dx, dy, dz = cx - cx0, cy - cy0, 0;
+dx, dy, dz = sx * dx, sy * dy, sz * dz;
+dx, dy = c_z * dx - s_z * dy, s_z * dx + c_z * dy;
+dz, dx = c_y * dz - s_y * dx, s_y * dz + c_y * dx;
+dy, dz = c_x * dy - s_x * dz, s_x * dy + c_x * dz;
+
+local x, y, frac = x0 + dx, y0 + dy, cx % 1;
+x, y =
+	math.floor(0.5 + x - frac) + frac,
+	math.floor(0.5 + y - frac) + frac;
+obj.ox, obj.oy = obj.ox + (x - x0), obj.oy + (y - y0);
+obj.cx, obj.cy = obj.cx + (cx - cx0), obj.cy + (cy - cy0);
